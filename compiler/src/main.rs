9d5ed9904ba51a9cadfb6a1791b405f7906f41eb
0918nobita <nobita.0918@gmail.com>
2020-07-20 12:45:08 +0900

    :construction: コマンドライン引数の一覧を出力する

--- compiler/src/main.rs
+++ compiler/src/main.rs
@@ -1,13 +1,18 @@
+use std::env;
 use std::fs::File;
 use std::io::{self, Write};
 use std::process::Command;
 
 fn main() -> io::Result<()> {
+    let args: Vec<String> = env::args().collect();
+    println!("{:?}", args);
+
     let mut file = File::create("./build.ninja")?;
     writeln!(
         file,
         "rule hello\n  command = echo \"HELLO\" > $out\n\nbuild hello.txt: hello"
     )?;
+
     Command::new("ninja").output()?;
     Ok(())
 }
