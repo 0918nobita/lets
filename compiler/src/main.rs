d37daf8a8f872634e63d081eac2acba777b0ec0f
0918nobita <nobita.0918@gmail.com>
2020-07-20 17:35:18 +0900

    :construction: ジェネリックな largest 関数を追加

--- compiler/src/main.rs
+++ compiler/src/main.rs
@@ -11,6 +11,15 @@ fn main() -> io::Result<()> {
     change(&mut s);
     println!("{}", s);
 
+    let word = first_word(&s);
+    println!("{}", word);
+
+    let num_list = vec![34, 50, 25, 100, 65];
+    println!("The largest number is {}", largest(&num_list));
+
+    let char_list = vec!['y', 'm', 'a', 'q'];
+    println!("The largest char is {}", largest(&char_list));
+
     fs::create_dir_all("./.lets-cache")?;
 
     let mut file = File::create("./.lets-cache/build.ninja")?;
@@ -27,4 +36,28 @@ fn main() -> io::Result<()> {
 
 fn change(some_string: &mut String) {
     some_string.push_str(", world");
+}
+
+fn first_word(s: &String) -> &str {
+    let bytes = s.as_bytes();
+
+    for (i, &item) in bytes.iter().enumerate() {
+        if item == b' ' {
+            return &s[0..i];
+        }
+    }
+
+    &s[..]
+}
+
+fn largest<T: PartialOrd + Copy>(list: &[T]) -> T {
+    let mut largest = list[0];
+
+    for &item in list.iter() {
+        if item > largest {
+            largest = item;
+        }
+    }
+
+    largest
 }
